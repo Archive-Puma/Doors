df682c8c4b6f674f5f837a695ada2fb4b3d71b8f
Sherlock Puma <kikefontanlorenzo@gmail.com>
2018-12-09 06:28:09 +0100

    :rocket: Different types!

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,11 @@
+#include "lib/types.h"
+
 // Entrypoint
 int kmain() {
-  char* vidmem=(char*)0xb8000;  // Video Memory Address
-  vidmem[0] = 'K';              // Print a letter
-  vidmem[1] = 0x04;             // Put a color
+  char* framebuffer=(char*)0x000B8000;  // Framebuffer (size: 25x80)
 
-  return 0;                     // Return Success Exit Code
+  framebuffer[0] = 'K';                 // Print a letter
+  framebuffer[1] = 0x04;                // Put a color
+
+  return 0;                             // Return Success Exit Code
 }
---
+++ src/lib/types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_INCLUDE
+#define TYPES_INCLUDE
+
+// String
+typedef char* string;
+
+// 8 bytes
+typedef signed char int8;
+typedef unsigned char uint8;
+
+// 16 bytes
+typedef signed short int16;
+typedef unsigned short uint16;
+
+// 32 bytes
+typedef signed int int32;
+typedef unsigned int uint32;
+
+#endif
